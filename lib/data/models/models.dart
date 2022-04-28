45f60fc8880e130a799c7293f68da8bf0b6dca7c
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-04-28 13:42:29 +0530

    feat: add Models
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

---
+++ lib/data/models/models.dart
@@ -0,0 +1,7 @@
+export 'package:gnucashx/data/models/account.dart';
+export 'package:gnucashx/data/models/book.dart';
+export 'package:gnucashx/data/models/commodity.dart';
+export 'package:gnucashx/data/models/guid.dart';
+export 'package:gnucashx/data/models/node.dart';
+export 'package:gnucashx/data/models/settings.dart';
+export 'package:gnucashx/data/models/slot.dart';
