965cda6dc841253750b7e117bc315342406fd2e0
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-04-28 10:03:16 +0530

    feat: add constants.dart
    * kNamepspace constants
    * kGUIDEncodingLength constant
    
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

---
+++ lib/utils/constants.dart
@@ -0,0 +1,38 @@
+/// Namespaces
+///
+/// Usage:
+///
+/// static const String prefix = kBook;
+/// static String? namespace = kNamespace[kBook];
+///
+const String kNode = 'node';
+const String kGnuCash = 'gnc';
+const String kAccount = 'act';
+const String kBook = 'book';
+const String kCountData = 'cd';
+const String kCommodity = 'cmdty';
+const String kPrice = 'price';
+const String kSlot = 'slot';
+const String kSplit = 'split';
+const String kScheduledTransaction = 'sx';
+const String kTransaction = 'trn';
+const String kBudget = 'bgt';
+
+const kNamespace = {
+  kNode: 'http://gnucashx.aakashhemadri.com/XML/node',
+  kGnuCash: 'http://www.gnucash.org/XML/gnc',
+  kAccount: 'http://www.gnucash.org/XML/act',
+  kBook: 'http://www.gnucash.org/XML/book',
+  kCountData: 'http://www.gnucash.org/XML/cd',
+  kCommodity: 'http://www.gnucash.org/XML/cmdty',
+  kPrice: 'http://www.gnucash.org/XML/price',
+  kSlot: 'http://www.gnucash.org/XML/slot',
+  kSplit: 'http://www.gnucash.org/XML/split',
+  kScheduledTransaction: 'http://www.gnucash.org/XML/sx',
+  kTransaction: 'http://www.gnucash.org/XML/trn',
+  kBudget: 'http://www.gnucash.org/XML/bgt',
+};
+
+/// GUID
+///
+const int kGUIDEncodingLength = 32;
