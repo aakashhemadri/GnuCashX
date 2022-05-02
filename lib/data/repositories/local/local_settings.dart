0aa7ad1717bec559cf295af049c02ae59291033b
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-02 17:19:16 +0530

    feat: add settings repository

---
+++ lib/data/repositories/local/local_settings.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:gnucashx/data/models/models.dart';
+import 'package:gnucashx/data/repositories/repositories.dart';
+import 'package:gnucashx/utils/constants.dart';
+
+class LocalSettingsRepo implements SettingsRepo {
+  @override
+  late Persistence source;
+
+  @override
+  Future<bool> reset() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String settings = jsonEncode(Settings.local());
+    return pref.setString(kSettingsJsonKey, settings);
+  }
+
+  @override
+  Future<bool> write(Settings settings) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String settingsJson = jsonEncode(settings.toJson());
+    return pref.setString(kSettingsJsonKey, settingsJson);
+  }
+
+  @override
+  Future<Settings> read() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String? settingsJson = pref.getString(kSettingsJsonKey);
+    if (settingsJson == null) {
+      await reset();
+      return Settings.local();
+    }
+    Map<String, dynamic> json =
+        jsonDecode(settingsJson) as Map<String, dynamic>;
+    return Settings.fromJson(json);
+  }
+
+  @override
+  Future<bool> backup() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> restore(Settings settings) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> sync(Settings settings) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/data/repositories/repositories.dart
@@ -0,0 +1,12 @@
+/// Repository Interface
+export 'package:gnucashx/data/repositories/interface_repo.dart';
+export 'package:gnucashx/data/repositories/settings_repo.dart';
+
+/// Local Repositories
+export 'package:gnucashx/data/repositories/local/local_settings.dart';
+
+/// SQLite Repositories
+export 'package:gnucashx/data/repositories/sqlite/sqlite_book.dart';
+
+/// XML Repositories
+export 'package:gnucashx/data/repositories/xml/xml_book.dart';
