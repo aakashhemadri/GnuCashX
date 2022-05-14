7c08f631b79ae189890501b042d1b12b53162023
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-15 02:08:43 +0530

    test: add == operator check
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- test/data/models/guid_test.dart
+++ test/data/models/guid_test.dart
@@ -4,16 +4,25 @@ import 'package:gnucashx/data/models/guid.dart';
 
 void main() {
   group('GUID:', () {
-    test('.from(String): Check GUID value', () {
-      /// Replace with XML retrieved guid.
-      String testUid = 'eb9dd351a71c493e842ad3d990fcff9d';
-      GUID guid = GUID(value: testUid);
-      expect(guid.value, testUid);
+    String sampleGuid = 'eb9dd351a71c493e842ad3d990fcff9d';
+    test('Check GUID value', () {
+      GUID guid = GUID(value: sampleGuid);
+      expect(guid.value, sampleGuid);
+    });
+
+    test('Check == operator', () {
+      expect(GUID(value: sampleGuid), GUID(value: sampleGuid));
     });
 
     test('.generate(): Check GUID encoding length', () {
       GUID guid = GUID.generate();
       expect(guid.value.length, kGUIDEncodingLength);
+    });
+
+    test('.generate(): Check if consecutive generate is unique', () {
+      GUID guid1 = GUID.generate();
+      GUID guid2 = GUID.generate();
+      expect(guid1 != guid2, true);
     });
   });
 }
