8a7974045ab41ec5269e0266ca453cb99e2c55fb
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-15 02:07:31 +0530

    fix: set defaults for favourite & hidden fields
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- lib/data/models/persistence.dart
+++ lib/data/models/persistence.dart
@@ -18,27 +18,27 @@ class Persistence with _$Persistence {
   @Assert('type == PersistenceType.local')
   const factory Persistence.local(
       {@Default(PersistenceType.local) PersistenceType type,
+      @Default(false) bool favourite,
+      @Default(false) bool hidden,
       required String key,
-      required bool favourite,
-      required bool hidden,
       required DateTime created,
       required DateTime modified}) = Local;
 
   @Assert('type == PersistenceType.xml')
   factory Persistence.xml(
       {@Default(PersistenceType.xml) PersistenceType type,
+      @Default(false) bool favourite,
+      @Default(false) bool hidden,
       required Uri uri,
-      required bool favourite,
-      required bool hidden,
       required DateTime created,
       required DateTime modified}) = XML;
 
   @Assert('type == PersistenceType.sqlite')
   factory Persistence.sqlite(
       {@Default(PersistenceType.sqlite) PersistenceType type,
+      @Default(false) bool favourite,
+      @Default(false) bool hidden,
       required Uri uri,
-      required bool favourite,
-      required bool hidden,
       required DateTime created,
       required DateTime modified}) = SQLite;
 
