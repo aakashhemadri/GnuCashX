d1ff43e30c6c43abea39b1465085e338959aa505
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-15 02:05:43 +0530

    feat: add freezed AccountType
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- lib/data/models/account.dart
+++ lib/data/models/account.dart
@@ -5,26 +5,49 @@ import 'package:gnucashx/data/models/models.dart';
 part 'account.freezed.dart';
 part 'account.g.dart';
 
-enum AccountType {
-  root,
-  asset,
-  equity,
-  liabiliy,
-  income,
-  expense,
-  bank,
+@Freezed()
+class AccountType with _$AccountType {
+  const factory AccountType._({
+    required String name,
+    required String type,
+  }) = _AccountType;
+
+  factory AccountType.root() => const AccountType._(name: 'Root', type: 'ROOT');
+  factory AccountType.bank() => const AccountType._(name: 'Bank', type: 'BANK');
+  factory AccountType.cash() => const AccountType._(name: 'Cash', type: 'CASH');
+  factory AccountType.asset() =>
+      const AccountType._(name: 'Asset', type: 'ASSET');
+  factory AccountType.credit() =>
+      const AccountType._(name: 'Credit Card', type: 'CREDIT');
+  factory AccountType.stock() =>
+      const AccountType._(name: 'Stock', type: 'STOCK');
+  factory AccountType.mutual() =>
+      const AccountType._(name: 'Mutal Fund', type: 'MUTUAL');
+  factory AccountType.income() =>
+      const AccountType._(name: 'Income', type: 'INCOME');
+  factory AccountType.expense() =>
+      const AccountType._(name: 'Expense', type: 'EXPENSE');
+  factory AccountType.equity() =>
+      const AccountType._(name: 'Equity', type: 'EQUITY');
+  factory AccountType.receivable() =>
+      const AccountType._(name: 'A/Receivable', type: 'RECEIVABLE');
+  factory AccountType.payable() =>
+      const AccountType._(name: 'A/Payable', type: 'PAYABLE');
+
+  factory AccountType.fromJson(Map<String, dynamic> json) =>
+      _$AccountTypeFromJson(json);
 }
 
 @Freezed()
 class Account extends Base with _$Account {
   const factory Account(
-      {required String guid,
+      {required GUID guid,
       required AccountType type,
       required String name,
       required GUID commodityGuid,
+      @Default(null) GUID? parentGuid,
       @Default(100) int commodityScu,
       @Default(0) int nonStdScu,
-      required GUID parentGuid,
       @Default('') String code,
       @Default('') String description,
       @Default(false) bool hidden,
