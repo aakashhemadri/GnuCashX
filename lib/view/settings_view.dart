ffe2eca61e95f28449c351a6f7ca4382c31c0939
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-21 00:43:14 +0530

    feat: add theme toggles to SettingsView
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- lib/view/settings_view.dart
+++ lib/view/settings_view.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:gnucashx/widget/widget.dart';
+import 'package:gnucashx/provider/provider.dart';
+import 'package:gnucashx/data/data.dart';
+import 'package:gnucashx/utils/utils.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({Key? key}) : super(key: key);
@@ -7,11 +13,94 @@ class SettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
-      body: Center(
-        child: Icon(
-          Icons.build_rounded,
-          size: 70,
-          color: Theme.of(context).colorScheme.secondaryContainer,
+      body: Consumer<SettingsProvider>(
+        builder: (context, settingsProvider, child) => ListView(
+          children: [
+            const ListTile(
+              title: Text('Sources'),
+              enabled: false,
+              dense: true,
+            ),
+            ListTile(
+              enabled: false,
+              title: const Text('Current GnuCash Book'),
+              subtitle: Builder(builder: (context) {
+                if (settingsProvider.settings.current == null) {
+                  return const Text('No sources available, tap to create.');
+                } else {
+                  return Text(settingsProvider.settings.current!.map(
+                      local: (Local value) => value.key,
+                      xml: (XML value) => value.uri.toString(),
+                      sqlite: (SQLite value) => value.uri.toString()));
+                }
+              }),
+              minLeadingWidth: kStyleMinLeadingWidth,
+              leading: const SizedBox(
+                  height: double.infinity, child: Icon(Icons.file_open)),
+            ),
+            kWidgetDivider,
+            const ListTile(
+              title: Text('Appearance'),
+              enabled: false,
+              dense: true,
+            ),
+            ListTile(
+              title: const Text('Dark Mode'),
+              subtitle: Builder(builder: (context) {
+                switch (settingsProvider.settings.theme) {
+                  case ThemeMode.system:
+                    return const Text('Follow system');
+                  case ThemeMode.light:
+                    return const Text('Light');
+                  case ThemeMode.dark:
+                    return const Text('Dark');
+                }
+              }),
+              onTap: () async => await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return SimpleDialog(
+                      title: const Text('Select Theme'),
+                      children: <Widget>[
+                        SimpleDialogOption(
+                          child: const Text('Follow system'),
+                          onPressed: () => Navigator.pop(context,
+                              settingsProvider.updateTheme(ThemeMode.system)),
+                        ),
+                        SimpleDialogOption(
+                          child: const Text('Light'),
+                          onPressed: () => Navigator.pop(context,
+                              settingsProvider.updateTheme(ThemeMode.light)),
+                        ),
+                        SimpleDialogOption(
+                          child: const Text('Dark'),
+                          onPressed: () => Navigator.pop(context,
+                              settingsProvider.updateTheme(ThemeMode.dark)),
+                        ),
+                      ],
+                    );
+                  }),
+            ),
+            SwitchListTile(
+                title: const Text('Dynamic Theme'),
+                value: settingsProvider.settings.isDynamic,
+                onChanged: (toggle) =>
+                    settingsProvider.switchDynamicTheme(toggle)),
+            kWidgetDivider,
+            const ListTile(
+              title: Text('EXPERIMENTAL'),
+              subtitle: Text('Unstable options, use with discretion.'),
+              enabled: false,
+              dense: true,
+            ),
+            SwitchListTile(
+                title: const Text('MaterialYou'),
+                subtitle: const Text('Enable Android S+ Theming'),
+                value: settingsProvider.settings.useMaterial3,
+                onChanged: (toggle) =>
+                    settingsProvider.switchMaterialVersion(toggle)),
+            kWidgetDivider,
+          ],
         ),
       ),
     );
