860899a7184fc44692fcffa1d8b933492c3433c2
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-20 16:25:58 +0530

    feat: add MoreView
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

---
+++ lib/view/more_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MoreView extends StatelessWidget {
+  const MoreView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('More')),
+    );
+  }
+}
