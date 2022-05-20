047cb56ae7aa2611901c3507017a92d6c83f882d
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-21 00:41:09 +0530

    feat: add support for dynamic and material3 themes
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:gnucashx/utils/theme.dart';
 import 'package:provider/provider.dart';
 
 import 'package:gnucashx/provider/provider.dart';
 import 'package:gnucashx/utils/constants.dart';
-import 'package:gnucashx/view/view.dart';
+import 'package:gnucashx/routes/routes.dart';
+import 'package:gnucashx/utils/theme.dart';
 
 final SettingsProvider settings = SettingsProvider();
 Future<void> main() async {
@@ -22,12 +23,33 @@ class GncApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<SettingsProvider>(
-      builder: (context, value, child) => MaterialApp(
-          title: kAppTitle,
-          home: const MainView(),
-          theme: lightTheme,
-          darkTheme: darkTheme,
-          themeMode: value.settings.theme),
-    );
+        builder: (context, settingsProvider, child) => DynamicColorBuilder(
+                builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+              ColorScheme lightColorScheme;
+              ColorScheme darkColorScheme;
+
+              if (lightDynamic != null &&
+                  darkDynamic != null &&
+                  settingsProvider.settings.isDynamic) {
+                lightColorScheme = lightDynamic.harmonized();
+                darkColorScheme = darkDynamic.harmonized();
+              } else {
+                lightColorScheme = brandLightColorScheme.harmonized();
+                darkColorScheme = brandDarkColorScheme.harmonized();
+              }
+
+              return MaterialApp(
+                title: kAppTitle,
+                initialRoute: RouteManager.mainPage,
+                onGenerateRoute: RouteManager.generateRoute,
+                theme: ThemeData(
+                    colorScheme: lightColorScheme,
+                    useMaterial3: settingsProvider.settings.useMaterial3),
+                darkTheme: ThemeData(
+                    colorScheme: darkColorScheme,
+                    useMaterial3: settingsProvider.settings.useMaterial3),
+                themeMode: settingsProvider.settings.theme,
+              );
+            }));
   }
 }
