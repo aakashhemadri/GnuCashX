4dbdb256fbd97c1b63eb7018932fcacedbbe8dec
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-21 00:41:09 +0530

    feat: add support for dynamic and material3 themes
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 
 // Color _brandColor = Color.fromRGBO(81, 205, 106, 1);
-Color _brandColor = Colors.green.shade700;
+Color _brandColor = Colors.green.shade50;
+// Color _brandColor = Colors.red.shade50;
 
-ThemeData lightTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: _brandColor,
-    brightness: Brightness.light,
-  ),
-  useMaterial3: true,
-);
+ColorScheme brandLightColorScheme =
+    ColorScheme.fromSeed(seedColor: _brandColor, brightness: Brightness.light);
 
-ThemeData darkTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: _brandColor,
-    brightness: Brightness.dark,
-  ),
-  useMaterial3: true,
-);
+ColorScheme brandDarkColorScheme =
+    ColorScheme.fromSeed(seedColor: _brandColor, brightness: Brightness.dark);
 
 // ThemeData dynamicTheme = ThemeData(colorScheme: ColorScheme.harmonized());
