f48fbed53e491935635107e904a5ea7d6ddbea7c
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-05-21 13:43:02 +0530

    chore: clean up theme.dart
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 
-// Color _brandColor = Color.fromRGBO(81, 205, 106, 1);
-Color _brandColor = Colors.green.shade50;
-// Color _brandColor = Colors.red.shade50;
+Color _brandColor = const Color.fromRGBO(81, 205, 106, 1);
 
 ColorScheme brandLightColorScheme =
     ColorScheme.fromSeed(seedColor: _brandColor, brightness: Brightness.light);
 
 ColorScheme brandDarkColorScheme =
     ColorScheme.fromSeed(seedColor: _brandColor, brightness: Brightness.dark);
-
-// ThemeData dynamicTheme = ThemeData(colorScheme: ColorScheme.harmonized());
